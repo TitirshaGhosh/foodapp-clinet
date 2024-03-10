e0fe636fe95e92e00407b0e98046c86db00dc2c6
TitirshaGhosh <titirsmail@gmail.com>
2024-03-10 22:52:46 +0530

    the hosting url2

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosPublic = axios.create({
   baseURL: "https://foodapp-server-5wwq.onrender.com",
 });
-
+ 
 const useAxiosPublic = () => {
   return axiosPublic;
 };
